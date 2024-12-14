012bf6cc8ec230ac4749208a0772fa84d97df039
Mohammad Hafiz <hafizmohan73@gmail.com>
2024-12-14 21:53:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ echo $var;
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<style>
-			<!-- style -->
+			/* style */
 		</style>
 	</head>
 	<body>
