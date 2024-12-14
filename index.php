32ad0a4ba7a69441c6b4b3633386b110610d88f6
Mohammad Hafiz <107490618+mohammadhafiizzz@users.noreply.github.com>
2024-12-14 21:50:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$var = "test";
+echo $var;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Tunku Abdul Rahman Marine Park</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style>
+			<!-- style -->
+		</style>
+	</head>
+	<body>
+		<header>
+			<!-- header -->
+		</header>
+		<main>
+			<!-- main content -->
+		</main>
+		<footer>
+			<!-- footer -->
+		</footer>
+	</body>
+</html>
